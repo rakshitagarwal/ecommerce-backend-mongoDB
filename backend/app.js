fd4eaad5476e249ed5327c1e2e5b88b8a7f54652
rakshitagarwal <rakshitag13@gmail.com>
2023-01-04 16:01:44 +0530

    task 4

--- backend/app.js
+++ backend/app.js
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
+  User.findById('63b54a2b089c17546da01555')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
