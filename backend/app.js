c2cfabad80d10d15929ed3b9f65cd8389ccb4617
rakshitagarwal <rakshitag13@gmail.com>
2023-01-06 16:01:26 +0530

    task 9

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('63b7f8111c03eda78a28fd8b')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,11 +34,23 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://rakshit:rakshit310@cluster0.qfsdpwi.mongodb.net/?retryWrites=true&w=majority'
+    "mongodb+srv://rakshit:rakshit310@cluster0.qfsdpwi.mongodb.net/?retryWrites=true&w=majority"
   )
-  .then(result => {
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Rakshit",
+          email: "rakshit@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
