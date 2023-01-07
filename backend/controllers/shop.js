eea794bb748be6f889bbe77a02e868ed1e0b3ffc
rakshitagarwal <rakshitag13@gmail.com>
2023-01-07 11:49:56 +0530

    task 11

--- backend/controllers/shop.js
+++ backend/controllers/shop.js
@@ -45,7 +45,7 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
-  
+
     .then(user => {
       const products = user.cart.items;
       res.render('shop/cart', {
@@ -72,7 +72,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
